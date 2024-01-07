f4070a60986f64af5b294dfd673e500c57effb4f
alonshrestha <alon.shrestha@cedargate.com>
2024-01-07 19:10:49 +0545

    initial commit iam key rotation

---
+++ config.py
@@ -0,0 +1,113 @@
+accountList = ['CGTAnalytics', 'CGTMaster', 'CGTCMProd', 'CGTECFProd', 'CGTNetworking', 'CGTCapAdjProd', 'CGTiClaims',
+               'CGTSouth', 'CGTEMR', 'CGTPipeline', 'CGTCorp', 'CGTBundlesProd', 'CGTECFNonProd', 'CGTBundlesNonProd',
+               'CGTCMNonProd', 'CGTDev', 'CGTUAT', 'CGTCapAdjNonProd', 'CGTBackup', 'CGTLogArchive',
+               'CGTSecurity', 'CGTAudit', 'CGTLogging', 'CGTDR', 'CGTSandbox']
+
+accountConfig = {
+    #  Production Accounts
+    "CGTAnalytics":
+        {
+            "id": "985867512284"
+        },
+    "CGTMaster":
+        {
+            "id": "185864130337"
+        },
+    "CGTCorp":
+        {
+            "id": "695387971665"
+        },
+    "CGTCMProd":
+        {
+            "id": "279177724563"
+        },
+    "CGTPipeline":
+        {
+            "id": "383530046460"
+        },
+    "CGTEMR":
+        {
+            "id": "467113185418"
+        },
+    "CGTSouth":
+        {
+            "id": "782391233053"
+        },
+    "CGTiClaims":
+        {
+            "id": "125006580046"
+        },
+    "CGTCapAdjProd":
+        {
+            "id": "072357485983"
+        },
+    "CGTNetworking":
+        {
+            "id": "551453947164"
+        },
+    "CGTDR":
+        {
+            "id": "235675868214"
+        },
+    "CGTECFProd":
+        {
+            "id": "764209023799"
+        },
+    "CGTBundlesProd":
+        {
+            "id": "942256427130"
+        },
+    "CGTSecurity":
+        {
+            "id": "355442602996"
+        },
+    "CGTLogging":
+        {
+            "id": "927098439544"
+        },
+    "CGTLogArchive":
+        {
+            "id": "633669109294"
+        },
+    "CGTAudit":
+        {
+            "id": "033015261166"
+        },
+    "CGTBackup":
+        {
+            "id": "269316322021"
+        },
+    #  Non-Production Accounts
+    "CGTSandbox":  # Tested
+        {
+            "id": "133171059739"
+        },
+    "CGTCapAdjNonProd":  # Tested
+        {
+            "id": "891525879289"
+        },
+    "CGTDev":  # Tested
+        {
+            "id": "137183587637"
+        },
+    "CGTUAT":  # Tested
+        {
+            "id": "211149015528"
+        },
+    "CGTEZCapNonProd":  # Tested
+        {
+            "id": "022336571218"
+        },
+    "CGTECFNonProd":  # Tested
+        {
+            "id": "211889649886"
+        },
+    "CGTBundlesNonProd":  # Tested
+        {
+            "id": "851853320826"
+        },
+    "CGTCMNonProd":  # Tested
+        {
+            "id": "241780692121"
+        }
+}
